f79521c050449edab66ea40ff3805c5c1dd1e637
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-28 11:17:03 +0700

    Add project files.

---
+++ ArrayListStuden/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayListStuden
+{
+    class Student
+    {
+        public string Name { set; get; }
+        public int Age { set; get; }
+
+       
+    }
+}
---
+++ ArrayListStuden/StudentManagement .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListStuden
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            ArrayList studentList = new ArrayList();
+            studentList.Add(new Student { Name = "Peter", Age = 20 });
+            studentList.Add(new Student { Name = "John", Age = 21 });
+            studentList.Add(new Student { Name = "Mark", Age = 19 });
+            foreach (Student student in studentList)
+                Console.WriteLine(student.Name + " " + student.Age);
+            Console.WriteLine("Thay doi phan tu trong ArrayList");
+            studentList[0] = new Student { Name = "Michael", Age = 21 };
+            foreach (Student student in studentList)
+              
+            Console.WriteLine(student.Name + " " + student.Age);
+            Console.WriteLine("Xoa phan tu trong ArrayList");
+            studentList.RemoveAt(2);
+            foreach (Student student in studentList)
+                Console.WriteLine(student.Name + " " + student.Age);
+        }
+    }
+}
